56fd7a0b509b1ba07b6f50eecb802b5a332aeea6
hanneslueke <johannes.lueke@siemens.com>
2024-02-08 14:45:26 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,119 @@ import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
 
+import { HighchartsChartModule } from 'highcharts-angular';
+import more from 'highcharts/highcharts-more';
+import * as Highcharts from 'highcharts/highstock';
+import exportingdata from 'highcharts/modules/export-data';
+import exporting from 'highcharts/modules/exporting';
+
+
+more(Highcharts);
+exporting(Highcharts);
+exportingdata(Highcharts);
+
+const DEFAULT_CONFIG: Highcharts.Options = {
+	chart: {
+		type: 'line',
+		zooming: {
+			type: 'x',
+		},
+	},
+	credits: {
+		enabled: false,
+	},
+	navigator: {
+		enabled: true,
+	},
+	legend: {
+		enabled: true,
+	},
+	plotOptions: {
+		series: {
+			turboThreshold: 20,
+			events: {
+				// Set Y-axis title to 0px to hide the title when the series is hidden
+				hide: function () {
+					this.yAxis.setTitle({ style: { fontSize: '0px' } });
+				},
+				// Set Y-axis title to 12px to show the title when the series is shown
+				show: function () {
+					this.yAxis.setTitle({ style: { fontSize: '12px' } });
+				},
+			},
+		},
+	},
+	tooltip: {
+		snap: 1,
+		enabled: true,
+	},
+	exporting: {
+		buttons: {
+			contextButton: {
+				menuItems: [
+					'toggle-tooltip',
+					'viewFullscreen',
+					'separator',
+					'downloadPNG',
+					'downloadSVG',
+					'downloadCSV',
+				],
+				symbol: 'menuball',
+			},
+		},
+	},
+	series: [],
+};
+
+Highcharts.setOptions({
+	time: {
+		timezoneOffset: new Date().getTimezoneOffset(),
+		useUTC: false,
+	},
+});
+
+
 @Component({
   selector: 'app-root',
+  import: [
+    HighchartsChartModule
+  ]
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+  <highcharts-chart
+    
+    [Highcharts]="Highcharts"
+    [constructorType]="chartConstructor"
+    [options]="chartOptions"
+    [callbackFunction]="chartCallback"
+    (chart)="(chart)"
+    [(update)]="updateFlag"
+    [oneToOne]="oneToOneFlag"
+    [runOutsideAngular]="runOutsideAngular"
+  >
+  </highcharts-chart>
+
   `,
 })
 export class App {
-  name = 'Angular';
+  chartOptions: Highcharts.Options = DEFAULT_CONFIG;
+	chart?: Highcharts.Chart;
+	chartCallback: Highcharts.ChartCallbackFunction;
+	Highcharts: typeof Highcharts = Highcharts; // required
+	chartConstructor = 'stockChart'; // optional string, defaults to 'chart'
+	updateFlag = false; // optional boolean
+	oneToOneFlag = false; // optional boolean, defaults to false
+	runOutsideAngular = false; // optional boolean, defaults to false
+
+
+  constructor(){
+    this.chartCallback =  (c: Highcharts.Chart) => {
+			if (this.chart == null) {
+				this.chart = c;
+			} 
+		};
+
+  }
 }
 
 bootstrapApplication(App);
